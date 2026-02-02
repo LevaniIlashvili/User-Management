860e5afc43f793c6e7d2482a10b504498273d970
Levan Ilashvili <levanilashvili2005@gmail.com>
2026-02-02 11:51:29 +0400

    add user entity

---
+++ UserManagement/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserManagement.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string Name { get; set; }
+    public DateTimeOffset LastLoginTime { get; set; }
+    public DateTimeOffset RegistrationTime { get; set; }
+    public bool IsBlocked { get; set; }
+}
